0ab903b1c051d70b2e5467197a8cdeab5b3d842e
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-08-12 15:56:19 +0400

    Basic Architecture

---
+++ src/js/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import '../scss/main.scss'
+
+function App() {
+
+    return (
+        <>
+            <h1>HELLO</h1>
+        </>
+    )
+}
+
+export default App
