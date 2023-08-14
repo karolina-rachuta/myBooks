14929ac7ab7d851aa46c5c5579fc4302aa74f050
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-08-14 13:04:51 +0400

    Added form, input, btn for search page with SCSS styles

--- src/js/pages/Search.jsx
+++ src/js/pages/Search.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const Search = () => {
+    const [search, setSearch] = useState("")
     return (
-        <div className= "pages">
-            <h1>Search</h1>
+        <div className="pages">
+            <div className="page__container">
+                <h1>Search</h1>
+                <form className="form__box">
+                    <input className="form__input" type="text" placeholder="Write title or author" value={search} onChange={(e)=> {setSearch(e.target.value)}}/>
+                    <button className="form__btn">Search</button>
+                </form>
+
+            </div>
+
         </div>
     )
 }
